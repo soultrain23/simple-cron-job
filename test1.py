be3a750967d29ecebcb75a390a9f788000d49923
newedition00 <99715952+newedition00@users.noreply.github.com>
2022-02-15 16:31:08 +0900

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+import urllib.request
+import time, os
+
+
+arr_url = os.environ["TARGET_URLS"]
+while(True):
+    try:
+        for url_ in arr_url:
+            open = urllib.request.urlopen(url=url_, timeout=60 * 3)
+            data = open.read().decode()
+            print(data)
+    except:
+        pass
+    time.sleep(30)
