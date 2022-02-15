ef0b6cd9db822bcba626e23f42384d381efcc152
newedition00 <99715952+newedition00@users.noreply.github.com>
2022-02-15 17:21:44 +0900

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,16 @@
+import urllib.request
+import time, os
+
+url1 = os.environ["URL1"]
+url2 = os.environ["URL2"]
+url3 = os.environ["URL3"]
+arr_url = [url1, url2, url3, ]
+while(True):
+    try:
+        for url_ in arr_url:
+            open = urllib.request.urlopen(url=url_, timeout=60 * 3)
+            data = open.read().decode()
+            print(data)
+    except:
+        pass
+    time.sleep(30)
