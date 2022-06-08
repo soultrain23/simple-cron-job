4bd4b2eab8ea435f58541c5164441f9aada1fcc9
soultrain23 <34236803+soultrain23@users.noreply.github.com>
2022-06-08 16:53:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import urllib.request
 import time, os
+import datetime
 
 url1 = os.environ["URL1"]
 url2 = os.environ["URL2"]
@@ -10,7 +11,18 @@ while(True):
         for url_ in arr_url:
             open = urllib.request.urlopen(url=url_, timeout=60 * 3)
             data = open.read().decode()
-            print(data)
-    except:
-        pass
-    time.sleep(30)
+            print(datetime.datetime.now(), url_)
+            time.sleep(5)
+    except Exception as e:
+        print(e)
+    try:
+        now_hour = int(str(datetime.datetime.now())[11:13])
+        if 2 <= now_hour < 7:
+            time.sleep(60 * 10)
+        elif 15 <= now_hour < 18:
+            time.sleep(60 * 10)
+        else:
+            time.sleep(60 * 5)
+    except Exception as e:
+        print(e)
+    time.sleep(1)
